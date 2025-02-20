aaa37dea9a78a6f2605d2d598d9f9109dd3acf5e
Killian0111 <45.killian.georges@gmail.com>
2025-02-20 09:50:12 +0100

    changement axios

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -77,7 +77,7 @@ export default function Profile() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Instagram
+                Website
               </a>
             </li>
           )}
