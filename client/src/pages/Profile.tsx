006100fbe1f29308c89066f502e8b56d14ddc436
Killian0111 <45.killian.georges@gmail.com>
2025-02-21 14:02:56 +0100

    userData fixed

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -6,54 +6,48 @@ export default function Profile() {
 
   return (
     <div className="profile">
-      {userData && (
-        <>
-          <img
-            src={
-              userData.profile_picture ||
-              "https://www.vhv.rs/dpng/d/138-1383989_default-svg-icon-free-avatar-png-transparent-png.png"
-            }
-            alt="pdeprofil"
-          />
-          <div className="username-followers">
-            <h1>{userData.username}</h1>
-            <p>Following: {userData.following}</p>
-            <p>Followers: {userData.followers}</p>
-          </div>
-          <p>Citations</p>
-          <div className="biography">
-            <h3>Biographie</h3>
-            <p>{userData.bio || "Aucune biographie renseignée."}</p>
-          </div>
-          <div className="links">
-            <h3>Liens</h3>
-            <ul className="list-none">
-              {userData.portfolio && (
-                <li>
-                  <a
-                    href={userData.portfolio}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Portfolio
-                  </a>
-                </li>
-              )}
-              {userData.website && (
-                <li>
-                  <a
-                    href={userData.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Website
-                  </a>
-                </li>
-              )}
-            </ul>
-          </div>
-        </>
-      )}
+      <>
+        <img
+          src={
+            userData.profile_picture ||
+            "https://www.vhv.rs/dpng/d/138-1383989_default-svg-icon-free-avatar-png-transparent-png.png"
+          }
+          alt="pdeprofil"
+        />
+        <div className="username-followers">
+          <h1>{userData.username}</h1>
+          <p>Following: {userData.following}</p>
+          <p>Followers: {userData.followers}</p>
+        </div>
+        <p>Citations</p>
+        <div className="biography">
+          <h3>Biographie</h3>
+          <p>{userData.bio || "Aucune biographie renseignée."}</p>
+        </div>
+        <div className="links">
+          <h3>Liens</h3>
+          <ul className="list-none">
+            <li>
+              <a
+                href={userData.portfolio ?? undefined}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a
+                href={userData.website ?? undefined}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Website
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
     </div>
   );
 }
