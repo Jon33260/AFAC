d0d0df48be5a13acb54114ba09629d065ef9ff95
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-03-04 16:52:13 +0100

    feat: add password hashing with argon2 for user security

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE user (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
+  hashed_password VARCHAR(255) NOT NULL,
   profile_picture VARCHAR(255) DEFAULT NULL,
   following INT DEFAULT 0,
   followers INT DEFAULT 0, 
@@ -12,11 +12,11 @@ CREATE TABLE user (
   website VARCHAR(255) DEFAULT NULL
 );
 
-INSERT INTO user(username, email, password, profile_picture, following, followers, bio, portfolio, website) VALUES
+INSERT INTO user(username, email, hashed_password, profile_picture, following, followers, bio, portfolio, website) VALUES
 ("Admin", "test@test.fr", "123456", "", 150, 230, "Développeur passionné par le web et les nouvelles technologies.", "https://johndoe.dev", "https://instagram.com/johndoe");
 
 
-INSERT INTO user(username, email, password, profile_picture, following, followers, bio, portfolio, website) VALUES
+INSERT INTO user(username, email, hashed_password, profile_picture, following, followers, bio, portfolio, website) VALUES
 ("Clément PICASSO", "test3@test.fr", "123456", "", 150, 230, "I am a digital artist and photographer capturing the beauty of Earth's diverse landscapes. Through photography and digital art, I blend reality and imagination to create immersive scenes that highlight nature's depth, light, and atmosphere, inviting viewers to explore the world through a new lens.", "https://johndoe.dev", "https://instagram.com/johndoe"),
 ("Admin", "admin@test.fr", "123456",NULL,0,0,NULL,NULL,NULL),
 ("Alice", "alice@example.com", "password1",NULL,0,0,NULL,NULL,NULL),
