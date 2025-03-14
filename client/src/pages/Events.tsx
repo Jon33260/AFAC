9a16f24a108188a37f8f17e5885e7d608c6da4dc
MaxenceGitDev <maxence.pruvot@icloud.com>
2025-03-14 14:11:30 +0100

    fixed css and responsive css event details

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -1,18 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import CurrentEvents from "../components/CurrentEvents";
 import UpcomingEvents from "../components/UpcomingEvents";
+import "../styles/Events.css";
 
 export default function Events() {
   const { currentEvents, upcomingEvents } = useLoaderData() as LoaderEvents;
 
   return (
     <>
-      <section className="currents">
-        <CurrentEvents currentEvents={currentEvents} />
-      </section>
-      <section className="upcoming">
-        <UpcomingEvents upcomingEvents={upcomingEvents} />
-      </section>
+      <div className="allevents-container">
+        <section className="currents">
+          <CurrentEvents currentEvents={currentEvents} />
+        </section>
+        <section className="upcoming">
+          <UpcomingEvents upcomingEvents={upcomingEvents} />
+        </section>
+      </div>
     </>
   );
 }
