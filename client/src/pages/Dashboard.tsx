cb853a8d0f87d0da4e3f6a614a57eed45059824a
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-03-18 17:27:49 +0100

    merge done

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -8,7 +8,9 @@ export default function Dashboard() {
   const { currentUser } = useAuth();
 
   if (!currentUser.is_admin) {
-    return <h1>Accès refusé !</h1>;
+    return (
+      <h1 style={{ marginTop: "5rem", marginLeft: "2rem" }}>Accès refusé !</h1>
+    );
   }
 
   return (
